56486fd6fb76cd3bfdf170463f330d209d5fec23
Yuya Orito <saoxxxv@gmail.com>
2025-07-19 21:35:34 +0900

    initial commit

---
+++ libs/libWFI.py
@@ -0,0 +1,198 @@
+import pandas as pa
+import numpy as na
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.ticker import MultipleLocator
+from matplotlib.lines import Line2D
+
+def add_WERs(strExcelFilename, sheetno, WER_a, WER_b, WER_k):
+    
+    # Excelファイルから指定されたシートのデータを読み込み、WER列を追加する関数。
+    
+    # :param strExcelFilename: Excelファイルのパス
+
+# # 引数Example
+# strExcelFilename = '/mnt/c/Users/saoxx/Documents/Develop/SNAr_charts_trim.xlsx'
+# WER_a = 1
+# WER_b = -9
+# WER_k = -10
+# sheetno = 0
+
+    try:
+        dfExcel = pa.read_excel(
+            strExcelFilename,
+            sheet_name=sheetno,
+            header=0
+        )
+    except ValueError as e:
+        print(f"Excelシートの読み込みでエラーが発生しました: {e}")
+        return None  # またはraiseや他のエラーハンドリング
+
+    # 1行目（iloc[0]）の値がNaNでない列を選択：ヘッダ行は数えないというちょっと許しがたい仕様
+    # 元のオブジェクトからのビューかオブジェクトのコピーかわからないから明示しないとだめらしい。ひどくない？
+    # つまりデフォルトの挙動はなくて文脈でポインタか実体かが決まってるってこと？ChatGPTによるとそうらしい。癖つよ。numpyの方が素直だって。
+    dfTrimmed = dfExcel.loc[:, dfExcel.iloc[0].notna()].copy()
+
+    target_columns = dfTrimmed.columns[dfTrimmed.iloc[0] == "Sim"]
+
+    for column in target_columns:
+        # add WER column
+        dfTrimmed.loc[2:, f"{column}_ER"] = dfTrimmed.loc[2:, column].astype(float).apply(
+            lambda x: ( WER_a + WER_b * ( ( na.exp( WER_k * x / 100 ) - 1 ) / WER_k ) ) * 100  # as x is percentile yield
+        )
+
+        # add Error upper column
+        dfTrimmed.loc[2:, f"{column}_EU"] = (
+            dfTrimmed.loc[2:, column].astype(float) * ( 1 + dfTrimmed.loc[2:, f"{column}_ER"].astype(float) / 100 )
+        )
+        
+        # add Error lower column
+        dfTrimmed.loc[2:, f"{column}_EL"] = (
+            dfTrimmed.loc[2:, column].astype(float) * ( 1 - dfTrimmed.loc[2:, f"{column}_ER"].astype(float) / 100 )
+        )
+
+    return dfTrimmed
+
+def draw_WERcharts(
+    dfTrimmed, 
+    var_marker_size, 
+    var_font_size, 
+    var_chart_size_inches_width, 
+    flagShowLegend, 
+    sim_startcolour, 
+    exp_startcolour
+):
+    
+    # Error handling.
+    if dfTrimmed is None or dfTrimmed.empty:
+        print("Invalid DataFrame provided.")
+        return None
+
+    # basic settings
+
+    plt.rcParams['font.family'] = 'Calibri'
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+
+    chart_colors = ['limegreen', 'orange', 'darkslateblue', 'magenta', 
+                    'forestgreen', 'indianred', 'royalblue', 'purple',
+                    'lawngreen', 'crimson', 'dodgerblue', 'deeppink']
+
+    #   プロットのオブジェクトを取得して基本設定
+    #   白銀比でプロットを作成
+
+    fig, ax = plt.subplots()
+    fig.set_dpi(400)        #default 100dpi
+    fig.set_size_inches(1.414*var_chart_size_inches_width,var_chart_size_inches_width)
+
+    #   Time系列の取得
+
+    time_column = dfTrimmed.columns[dfTrimmed.iloc[0].astype(str).str.match(r"^[tT]ime\s*")]
+
+    #このままだとTime系列の型がobjectになってるのでfloatに変換する必要がある
+    #   シミュレーションデータ+WERの描画
+    #   ソートしようと思ったがあえてしない。ユーザに左から並べさせる。
+
+    simulation_columns = dfTrimmed.columns[dfTrimmed.iloc[0] == "Sim"]
+    experiment_columns = dfTrimmed.columns[dfTrimmed.iloc[0] == "Exp"]
+
+    dfDraw = dfTrimmed.drop(index=[0,1])  # drop header rows
+    dfDraw[f"{time_column[0]}"] = dfDraw[f"{time_column[0]}"].astype(float)  # convert time column to float
+
+
+    #   シミュレーションデータの描画
+
+    color_index = sim_startcolour % len(chart_colors)  # ensure color index is within bounds
+
+    for column in simulation_columns:
+        dfDraw[f"{column}"] = dfDraw[f"{column}"].astype(float)  # convert simulation column to float
+        
+        ax.fill_between(                                    #塗りつぶし
+                    #time_column,
+                    dfDraw[f"{time_column[0]}"],            #X
+                    dfDraw[f"{column}_EU"],                 #Y1
+                    dfDraw[f"{column}_EL"],                 #Y2
+                    color=chart_colors[color_index],
+                    alpha=0.2,
+                    edgecolor="none"
+        )
+
+        dfDraw.plot( ax=ax,
+                    x=f"{time_column[0]}",
+                    y=f"{column}",
+                    grid=True,
+                    color=chart_colors[color_index],
+                    style="-",
+                    alpha=0.7
+        )
+
+        color_index += 1
+        color_index %= len(chart_colors)  # ensure color index is within bounds
+
+    #   実験データの描画
+
+    color_index = exp_startcolour % len(chart_colors)  # ensure color index is within bounds
+    custom_lines = []  # for legend
+
+    for column in experiment_columns:
+        dfDraw[f"{column}"] = dfDraw[f"{column}"].astype(float)  # convert experiment column to float
+        
+        dfDraw.plot( ax=ax,
+                    x=f"{time_column[0]}",
+                    y=f"{column}",
+                    grid=True,
+                    color=chart_colors[color_index],
+                    marker="o",
+                    markersize=var_marker_size,
+                    linestyle='None',
+                    alpha=1.0
+        )
+
+        #   凡例の設定：まったく新しいLine2Dオブジェクトのセットを作ることで好きなように凡例内を変える
+        
+        custom_lines=custom_lines + [
+            Line2D([0], [0], color=chart_colors[color_index], marker='o', markersize=var_marker_size, linestyle='-', label=r"$"+column+r"$")
+        ]
+
+        color_index += 1
+        color_index %= len(chart_colors)
+
+    #   凡例の表示
+
+    legend=ax.legend(handles=custom_lines, loc='best', ncol=1, fontsize=var_font_size-1)
+    f = legend.get_frame()
+    f.set_alpha(1)
+
+    ax.get_legend().set_visible(flagShowLegend)
+        
+    #   目盛線の設定
+
+    for i in ax.spines.values():
+        i.set_color('dimgrey')
+
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+
+    ax.tick_params(axis='both',which='both', colors='gainsboro', labelsize=var_font_size-1)
+    ax.grid(color='gainsboro', linestyle='-', linewidth=0.5)
+
+
+    for l in ax.get_xticklabels():
+        l.set_color('dimgrey')
+
+    for l in ax.get_yticklabels():
+        l.set_color('dimgrey')
+
+    #  軸の設定
+
+    ax.set_xlabel(r"Time, min", loc='right', color='dimgrey', fontsize=var_font_size, labelpad=2)       #r""はraw literal、エスケープ抑止
+    ax.set_ylabel(r"Yield, %", loc="top", color='dimgrey', fontsize=var_font_size, labelpad=2)
+
+    ax.set_xlim(left=0)
+    ax.set_ylim((0,105))
+
+    return fig, ax
+
+
+
+
---
+++ src/sttest.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__),".."))
+
+from libs.libWFI import add_WERs, draw_WERcharts
+
+
+st.title("Chart Drawer test")
+
+with st.expander("Terms and Conditions"):
+    st.markdown("""
+    * **For non-commercial use:** free to use with appropriate citation.              
+    Cite this:  
+    Orito, Y.  _ACS Omega_ **2025**, _10_ (9), 9266–9274. https://doi.org/10.1021/acsomega.4c09609.  
+    * **For commercial use:** please contact the author.
+                """)
+
+uploaded_file = st.sidebar.file_uploader("Upload an Excel file", type=["xlsx"])
+if uploaded_file is not None:
+    sheet_name = st.sidebar.number_input("Sheet index No", value=0)
+    st.sidebar.header("WFI Parameters")
+    WER_a = st.sidebar.number_input("WER_a", value=1.0)
+    WER_b = st.sidebar.number_input("WER_b", value=-9.0)
+    WER_k = st.sidebar.number_input("WER_k", value=-10.0)
+    st.sidebar.header("Chart drawing settings")
+    var_marker_size = st.sidebar.number_input("Marker Size", value=5)
+    var_font_size = st.sidebar.number_input("Font Size", value=10)
+    var_chart_size_inches_width = st.sidebar.number_input("Chart Width (inches)", value=3)
+    flagShowLegend = st.sidebar.checkbox("Show Legend", value=True)
+    colour_preset_indices = {0: "A", 4: "B", 8: "C"}
+    sim_startcolour = st.sidebar.selectbox("Simulation Start Color Preset", options=list(colour_preset_indices.keys()), index=0, format_func=lambda x: colour_preset_indices[x])
+    exp_startcolour = st.sidebar.selectbox("Experiment Start Color Preset", options=list(colour_preset_indices.keys()), index=0, format_func=lambda x: colour_preset_indices[x])
+
+    if st.button("Process"):
+        df = pd.read_excel(uploaded_file, sheet_name=sheet_name)
+        df_trimmed = add_WERs(uploaded_file, sheet_name, WER_a, WER_b, WER_k)
+        
+        fig, ax = draw_WERcharts(
+            df_trimmed, 
+            var_marker_size, 
+            var_font_size, 
+            var_chart_size_inches_width, 
+            flagShowLegend, 
+            sim_startcolour, 
+            exp_startcolour
+        )
+
+        st.pyplot(fig)
